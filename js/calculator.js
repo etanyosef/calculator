8c236d968c7c7ce7dd9573b7e0654b14d21e0e24
Ethan Joseph <ethancredo.ascc@outlook.com>
2025-11-10 00:08:35 +0800

    calculate when user add new operator without click equals

--- js/calculator.js
+++ js/calculator.js
@@ -34,9 +34,18 @@ function operate(num1, num2, operator) {
 
 let calcScreen = document.querySelector('.calc-screen');
 let calcMiniScreen = document.querySelector('.calc-mini-screen');
-let num1 = 0;
-let num2 = 0;
+let num1 = '';
+let num2 = '';
 let operator;
+
+const clearAll = document.querySelector('.clear-all');
+clearAll.addEventListener('click', () => {
+    calcScreen.value = '';
+    calcMiniScreen.value = '';
+    num1 = 0;
+    num2 = 0;
+    operator = '';
+});
 
 let numKey = document.querySelectorAll('.num-key');
 numKey.forEach( (key) => {
@@ -48,25 +57,39 @@ numKey.forEach( (key) => {
 let operatorKey = document.querySelectorAll('.operator-key');
 operatorKey.forEach( (key) => {
     key.addEventListener('click', () => {
-        if ( !(calcScreen.value === '') ) {
-            num1 = calcScreen.value;
-        operator = key.textContent;
-
-        calcMiniScreen.value = `${num1} ${operator}`;
-        calcScreen.value = "";
-        }
         
+        if(!(calcMiniScreen.value === '') && !(calcScreen.value === '')) {
+            // num1 = num2;
+            num2 = calcScreen.value;
+            
+            console.log(`${num1} ${operator} ${num2}`);
+            let result = operate(parseInt(num1), parseInt(num2), operator);
+            calcMiniScreen.value = `${result} ${operator}`;
+            num1 = result;
+            calcScreen.value = '';
+            operator = key.textContent;
+        } else {
+            num1 = calcScreen.value;
+            operator = key.textContent;
+            calcMiniScreen.value += ` ${num1} ${operator}`;
+            calcScreen.value = "";
+            // // num2 = calcScreen.value;
+            // console.log(`${num1} ${operator} ${num2}`);
+        }
+
     });
 });
 
 let equalsKey = document.querySelector('.equals-key');
 equalsKey.addEventListener('click', () => {
     // do nothing if user click equals without 2nd number
-    if ( !(num1 === 0) && !(calcScreen.value === '') ) {
+    if ( !(num1 === 0) && !(calcScreen.value === '') && !(calcMiniScreen.value === '') ) {
         num2 = calcScreen.value;
         calcScreen.value = operate(parseInt(num1), parseInt(num2), operator);
-        calcMiniScreen.value += ` ${num2} =`;
+        calcMiniScreen.value = `${num1} ${operator} ${num2} =`;
         console.log(`${num1} ${operator} ${num2}`);
+        console.log(`num1 = ${num1}, num2 = ${num2}`);
+        num2 = num1;
         num1 = 0;
     } 
 
